0cdad6516a5d42391d754a830cf9f03cf474fecb
Mostafa Mahmoud Salem <msalem@ejada.com>
2020-12-11 01:31:32 +0200

    feat: added search bar handling

--- app/src/main/java/com/example/robustasearchdemo/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/robustasearchdemo/ui/activity/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.robustasearchdemo.ui.activity
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.example.robustasearchdemo.R
 import com.example.robustasearchdemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +23,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        handleIntent(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        (menu?.findItem(R.id.search)?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+            val handler = Handler()
+            setOnQueryTextListener(object : OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(query: String): Boolean {
+                    handler.removeCallbacksAndMessages(null)
+                    handler.postDelayed({
+                        Log.d(localClassName, "Delayed query: $query")
+                    }, USER_TYPING_DELAY_PERIOD)
+                    return false
+                }
+            })
+        }
+        return true
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            Log.d(localClassName, "Intent Query: $query")
+        }
+    }
+
+    companion object {
+        const val USER_TYPING_DELAY_PERIOD: Long = 400
     }
 }
